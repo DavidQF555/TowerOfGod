06b9f3ead09289fcacaee5a150df8ba6259ae5ba
DavidQF555 <davidfeng2003@gmail.com>
2020-07-27 16:54:14 -0700

    Configured Mod Frame

---
+++ src/main/java/com/davidqf/towerofgod/TowerOfGod.java
@@ -0,0 +1,21 @@
+package com.davidqf.towerofgod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.*;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("towerofgod")
+public class TowerOfGod {
+
+	public TowerOfGod() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {}
+
+	private void doClientStuff(final FMLClientSetupEvent event) {}
+}
