4badee660d55a5a0c180003d338678919d94341d
DavidQF555 <davidfeng2003@gmail.com>
2020-07-28 03:46:28 -0700

    Changed Package Name

--- src/main/java/com/davidqf/towerofgodmod/TowerOfGod.java
+++ src/main/java/com/davidqf/towerofgodmod/TowerOfGod.java
@@ -1,21 +1,21 @@
-package com.davidqf.towerofgod;
+package com.davidqf.towerofgodmod;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("towerofgod")
+@Mod("towerofgodmod")
 public class TowerOfGod {
 
 	public TowerOfGod() {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	private void setup(final FMLCommonSetupEvent event) {}
 
 	private void doClientStuff(final FMLClientSetupEvent event) {}
+
 }
