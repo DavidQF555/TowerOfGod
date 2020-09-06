34836c8c19c606de1a19a77fb132fd08310b5632
DavidQF555 <davidfeng2003@gmail.com>
2020-09-06 04:56:27 -0700

    Fixed Regular Spawn Locations

--- src/main/java/com/davidqf/minecraft/towerofgod/world/gen/EntitySpawnGen.java
+++ src/main/java/com/davidqf/minecraft/towerofgod/world/gen/EntitySpawnGen.java
@@ -15,7 +15,7 @@ public class EntitySpawnGen {
     @SubscribeEvent
     public static void spawnEntities(FMLLoadCompleteEvent event) {
         for (Biome biome : ForgeRegistries.BIOMES) {
-            if (biome.getCategory() != Biome.Category.THEEND && biome.getCategory() != Biome.Category.NETHER) {
+            if (biome.getCategory() != Biome.Category.THEEND && biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.RIVER) {
                 biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(RegistryHandler.REGULAR_ENTITY.get(), 8, 1, 8));
             }
         }
