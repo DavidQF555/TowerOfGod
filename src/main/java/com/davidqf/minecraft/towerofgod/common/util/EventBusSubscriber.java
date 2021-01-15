beae10a150ed3d8901cc5550dfe678edf919f7b7
DavidQF555 <davidfeng2003@gmail.com>
2021-01-14 23:43:50 -0800

    Moved World Event Subscribers

--- src/main/java/com/davidqf/minecraft/towerofgod/common/util/EventBusSubscriber.java
+++ src/main/java/com/davidqf/minecraft/towerofgod/common/util/EventBusSubscriber.java
@@ -8,9 +8,17 @@ import com.davidqf.minecraft.towerofgod.common.items.ShinsuItemColor;
 import com.davidqf.minecraft.towerofgod.common.packets.*;
 import com.davidqf.minecraft.towerofgod.common.entities.ShinsuUserEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -19,38 +27,59 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod.EventBusSubscriber(modid = TowerOfGod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class CapabilityHandler {
+public class EventBusSubscriber {
 
     private static final ResourceLocation SHINSU_STATS = new ResourceLocation(TowerOfGod.MOD_ID, "shinsu_stats");
     private static final ResourceLocation PLAYER_EQUIPS = new ResourceLocation(TowerOfGod.MOD_ID, "player_equips");
     private static int index = 0;
 
-    @SubscribeEvent
-    public static void attachCapability(AttachCapabilitiesEvent<Entity> event) {
-        Entity entity = event.getObject();
-        if (entity instanceof ShinsuUserEntity || entity instanceof PlayerEntity) {
-            event.addCapability(SHINSU_STATS, new IShinsuStats.Provider());
-        }
-        if (entity instanceof PlayerEntity) {
-            event.addCapability(PLAYER_EQUIPS, new IPlayerShinsuEquips.Provider());
-        }
-    }
+    @Mod.EventBusSubscriber(modid = TowerOfGod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class ForgeBus {
 
-    @SubscribeEvent
-    public static void onRegisterCommands(RegisterCommandsEvent event) {
-        StatsCommand.register(event.getDispatcher());
+        @SubscribeEvent
+        public static void attachCapability(AttachCapabilitiesEvent<Entity> event) {
+            Entity entity = event.getObject();
+            if (entity instanceof ShinsuUserEntity || entity instanceof PlayerEntity) {
+                event.addCapability(SHINSU_STATS, new IShinsuStats.Provider());
+            }
+            if (entity instanceof PlayerEntity) {
+                event.addCapability(PLAYER_EQUIPS, new IPlayerShinsuEquips.Provider());
+            }
+        }
+
+        @SubscribeEvent
+        public static void onRegisterCommands(RegisterCommandsEvent event) {
+            StatsCommand.register(event.getDispatcher());
+        }
+
     }
 
     @Mod.EventBusSubscriber(modid = TowerOfGod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    private static class ModBus {
+    public static class ModBus {
 
         @SubscribeEvent
         public static void onHandleColors(ColorHandlerEvent.Item event) {
             ShinsuItemColor color = new ShinsuItemColor();
             for (RegistryObject<? extends Item> item : RegistryHandler.SHINSU_ITEMS) {
                 event.getItemColors().register(color, item::get);
+            }
+        }
+
+        @SubscribeEvent
+        public static void onFMLLoadComplete(FMLLoadCompleteEvent event) {
+            for (Biome biome : ForgeRegistries.BIOMES) {
+                if (biome.getCategory() != Biome.Category.THEEND && biome.getCategory() != Biome.Category.NETHER) {
+                    CountRangeConfig range = new CountRangeConfig(3, 17, 0, 100);
+                    OreFeatureConfig feature = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, RegistryHandler.SUSPENDIUM_ORE.get().getDefaultState(), 8);
+                    ConfiguredPlacement<CountRangeConfig> config = Placement.COUNT_RANGE.configure(range);
+                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(feature).withPlacement(config));
+                    if (biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.RIVER) {
+                        biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(RegistryHandler.REGULAR_ENTITY.get(), 8, 1, 8));
+                    }
+                }
             }
         }
 
