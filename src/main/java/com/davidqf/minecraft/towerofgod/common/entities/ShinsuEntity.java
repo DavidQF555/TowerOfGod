c79b362d555172bc2a3cfc4f79b1faef4cd6bdda
DavidQF555 <davidfeng2003@gmail.com>
2021-01-14 20:57:23 -0800

    Fixed Shinsu Entity Collisions

--- src/main/java/com/davidqf/minecraft/towerofgod/common/entities/ShinsuEntity.java
+++ src/main/java/com/davidqf/minecraft/towerofgod/common/entities/ShinsuEntity.java
@@ -78,11 +78,10 @@ public class ShinsuEntity extends DamagingProjectileEntity {
     public void onEntityHit(EntityRayTraceResult rayTraceResult) {
         super.onEntityHit(rayTraceResult);
         if (world instanceof ServerWorld) {
-            Entity e = rayTraceResult.getEntity();
             Entity shooter = func_234616_v_();
-            LivingEntity target = (LivingEntity) e;
+            Entity target = rayTraceResult.getEntity();
             ShinsuQuality quality = getQuality();
-            float damage = (float) (IShinsuStats.getTotalResistance(shooter, target) * level * quality.getDamage() * DAMAGE) / 3;
+            float damage = (float) ((shooter == null) ? level * quality.getDamage() * DAMAGE : IShinsuStats.getTotalResistance(shooter, target) * level * quality.getDamage() * DAMAGE) / 3;
             quality.applyEntityEffect(this, rayTraceResult);
             target.attackEntityFrom(quality.getSource(), damage);
         }
