5a0d0d2900928cdd9337e15aae0510920800bf73
DavidQF555 <davidfeng2003@gmail.com>
2021-09-30 01:10:17 -0700

    Improved Shinsu Command

--- src/main/java/io/github/davidqf555/minecraft/towerofgod/common/commands/ShinsuCommand.java
+++ src/main/java/io/github/davidqf555/minecraft/towerofgod/common/commands/ShinsuCommand.java
@@ -2,16 +2,10 @@ package io.github.davidqf555.minecraft.towerofgod.common.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import io.github.davidqf555.minecraft.towerofgod.common.TowerOfGod;
 import io.github.davidqf555.minecraft.towerofgod.common.capabilities.ShinsuStats;
-import io.github.davidqf555.minecraft.towerofgod.common.entities.IShinsuUser;
 import io.github.davidqf555.minecraft.towerofgod.common.packets.UpdateClientKnownPacket;
-import io.github.davidqf555.minecraft.towerofgod.common.packets.UpdateStatsMetersPacket;
 import io.github.davidqf555.minecraft.towerofgod.common.techinques.ShinsuTechnique;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
@@ -20,11 +14,11 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.server.command.EnumArgument;
 
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 public class ShinsuCommand {
 
@@ -34,108 +28,120 @@ public class ShinsuCommand {
     private static final String BAANGS = "commands." + TowerOfGod.MOD_ID + ".shinsu.baangs";
     private static final String RESISTANCE = "commands." + TowerOfGod.MOD_ID + ".shinsu.resistance";
     private static final String TENSION = "commands." + TowerOfGod.MOD_ID + ".shinsu.tension";
-    private static final String TECHNIQUE_SUCCESS = "commands." + TowerOfGod.MOD_ID + ".shinsu.technique_success";
-    private static final String TECHNIQUE_FAIL = "commands." + TowerOfGod.MOD_ID + ".shinsu.technique_fail";
+    private static final String TECHNIQUE = "commands." + TowerOfGod.MOD_ID + ".shinsu.technique";
 
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(Commands.literal("shinsu")
                 .requires(source -> source.hasPermissionLevel(2))
-                .then(Commands.argument("targets", EntityArgument.players())
-                        .then(Commands.argument("type", StringArgumentType.word())
-                                .suggests(new TypeSuggestionProvider())
-                                .executes(context -> execute(context.getSource(), EntityArgument.getEntities(context, "targets"), StringArgumentType.getString(context, "type")))
-                                .then(Commands.argument("amount", DoubleArgumentType.doubleArg())
-                                        .executes(context -> execute(context.getSource(), EntityArgument.getEntities(context, "targets"), StringArgumentType.getString(context, "type"), DoubleArgumentType.getDouble(context, "amount")))))
+                .then(Commands.argument("targets", EntityArgument.entities())
+                        .then(Commands.literal("level")
+                                .then(Commands.argument("value", IntegerArgumentType.integer())
+                                        .executes(context -> changeLevel(context.getSource(), EntityArgument.getEntities(context, "targets"), IntegerArgumentType.getInteger(context, "value")))
+                                )
+                        )
+                        .then(Commands.literal("shinsu")
+                                .then(Commands.argument("value", IntegerArgumentType.integer())
+                                        .executes(context -> changeShinsu(context.getSource(), EntityArgument.getEntities(context, "targets"), IntegerArgumentType.getInteger(context, "value")))
+                                )
+                        )
+                        .then(Commands.literal("baangs")
+                                .then(Commands.argument("value", IntegerArgumentType.integer())
+                                        .executes(context -> changeBaangs(context.getSource(), EntityArgument.getEntities(context, "targets"), IntegerArgumentType.getInteger(context, "value")))
+                                )
+                        )
+                        .then(Commands.literal("resistance")
+                                .then(Commands.argument("factor", DoubleArgumentType.doubleArg())
+                                        .executes(context -> changeResistance(context.getSource(), EntityArgument.getEntities(context, "targets"), DoubleArgumentType.getDouble(context, "factor")))
+                                )
+                        )
+                        .then(Commands.literal("tension")
+                                .then(Commands.argument("factor", DoubleArgumentType.doubleArg())
+                                        .executes(context -> changeTension(context.getSource(), EntityArgument.getEntities(context, "targets"), DoubleArgumentType.getDouble(context, "factor")))
+                                )
+                        )
+                        .then(Commands.literal("technique")
+                                .then(Commands.argument("type", EnumArgument.enumArgument(ShinsuTechnique.class))
+                                        .executes(context -> changeTechniqueLevel(context.getSource(), EntityArgument.getEntities(context, "targets"), context.getArgument("type", ShinsuTechnique.class)))
+                                        .then(Commands.argument("value", IntegerArgumentType.integer())
+                                                .executes(context -> changeTechniqueLevel(context.getSource(), EntityArgument.getEntities(context, "targets"), context.getArgument("type", ShinsuTechnique.class), IntegerArgumentType.getInteger(context, "value")))
+                                        )
+                                )
+                        )
                 )
         );
     }
 
-    private static int execute(CommandSource source, Collection<? extends Entity> entities, String type) {
-        return execute(source, entities, type, 1);
+    private static int changeLevel(CommandSource source, Collection<? extends Entity> entities, int change) {
+        for (Entity entity : entities) {
+            ShinsuStats.get(entity).addLevel(change);
+            source.sendFeedback(new TranslationTextComponent(LEVEL, entity.getDisplayName(), change), true);
+        }
+        return entities.size();
     }
 
-    private static int execute(CommandSource source, Collection<? extends Entity> entities, String type, double amount) {
-        if (amount == 0) {
+    private static int changeShinsu(CommandSource source, Collection<? extends Entity> entities, int change) {
+        for (Entity entity : entities) {
+            ShinsuStats.get(entity).addMaxShinsu(change);
+            source.sendFeedback(new TranslationTextComponent(SHINSU, entity.getDisplayName(), change), true);
+        }
+        return entities.size();
+    }
+
+    private static int changeBaangs(CommandSource source, Collection<? extends Entity> entities, int change) {
+        for (Entity entity : entities) {
+            ShinsuStats.get(entity).addMaxBaangs(change);
+            source.sendFeedback(new TranslationTextComponent(BAANGS, entity.getDisplayName(), change), true);
+        }
+        return entities.size();
+    }
+
+    private static int changeResistance(CommandSource source, Collection<? extends Entity> entities, double factor) {
+        if (factor == 0) {
             source.sendErrorMessage(ZERO);
             return 0;
-        } else {
-            int fail = 0;
-            for (Entity entity : entities) {
-                if (entity instanceof IShinsuUser || entity instanceof ServerPlayerEntity) {
-                    ShinsuStats stats = ShinsuStats.get(entity);
-                    switch (type.toLowerCase()) {
-                        case "level":
-                            stats.addLevel((int) amount);
-                            source.sendFeedback(new TranslationTextComponent(LEVEL, entity.getDisplayName(), (int) amount), true);
-                            break;
-                        case "shinsu":
-                            stats.addMaxShinsu((int) amount);
-                            if (entity instanceof ServerPlayerEntity) {
-                                TowerOfGod.CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity), new UpdateStatsMetersPacket(stats.getShinsu(), stats.getMaxShinsu(), stats.getBaangs(), stats.getMaxBaangs()));
-                            }
-                            source.sendFeedback(new TranslationTextComponent(SHINSU, entity.getDisplayName(), (int) amount), true);
-                            break;
-                        case "baangs":
-                            stats.addMaxBaangs((int) amount);
-                            if (entity instanceof ServerPlayerEntity) {
-                                TowerOfGod.CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity), new UpdateStatsMetersPacket(stats.getShinsu(), stats.getMaxShinsu(), stats.getBaangs(), stats.getMaxBaangs()));
-                            }
-                            source.sendFeedback(new TranslationTextComponent(BAANGS, entity.getDisplayName(), (int) amount), true);
-                            break;
-                        case "resistance":
-                            if (amount < 0) {
-                                amount = -1 / amount;
-                            }
-                            stats.multiplyBaseResistance(amount);
-                            source.sendFeedback(new TranslationTextComponent(RESISTANCE, entity.getDisplayName(), amount), true);
-                            break;
-                        case "tension":
-                            if (amount < 0) {
-                                amount = -1 / amount;
-                            }
-                            stats.multiplyBaseTension(amount);
-                            source.sendFeedback(new TranslationTextComponent(TENSION, entity.getDisplayName(), amount), true);
-                            break;
-                        default:
-                            ShinsuTechnique technique;
-                            try {
-                                technique = ShinsuTechnique.valueOf(type.toUpperCase());
-                            } catch (IllegalArgumentException exception) {
-                                source.sendErrorMessage(new TranslationTextComponent(TECHNIQUE_FAIL, entity.getDisplayName(), type));
-                                fail++;
-                                break;
-                            }
-                            stats.addKnownTechnique(technique, (int) amount);
-                            if (entity instanceof ServerPlayerEntity) {
-                                Map<ShinsuTechnique, Integer> known = new EnumMap<>(ShinsuTechnique.class);
-                                for (ShinsuTechnique t : ShinsuTechnique.values()) {
-                                    known.put(t, stats.getTechniqueLevel(t));
-                                }
-                                TowerOfGod.CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity), new UpdateClientKnownPacket(known));
-                            }
-                            source.sendFeedback(new TranslationTextComponent(TECHNIQUE_SUCCESS, entity.getDisplayName(), technique.getText(), (int) amount), true);
-                    }
-                } else {
-                    fail++;
-                }
-            }
-            return entities.size() - fail;
         }
+        if (factor < 0) {
+            factor = -1 / factor;
+        }
+        for (Entity entity : entities) {
+            ShinsuStats.get(entity).multiplyBaseResistance(factor);
+            source.sendFeedback(new TranslationTextComponent(RESISTANCE, entity.getDisplayName(), factor), true);
+        }
+        return entities.size();
     }
 
-    private static class TypeSuggestionProvider implements SuggestionProvider<CommandSource> {
-
-        @Override
-        public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSource> context, SuggestionsBuilder builder) {
-            builder.suggest("level");
-            builder.suggest("shinsu");
-            builder.suggest("baangs");
-            builder.suggest("resistance");
-            builder.suggest("tension");
-            for (ShinsuTechnique technique : ShinsuTechnique.getObtainableTechniques()) {
-                builder.suggest(technique.name().toLowerCase());
-            }
-            return builder.buildFuture();
+    private static int changeTension(CommandSource source, Collection<? extends Entity> entities, double factor) {
+        if (factor == 0) {
+            source.sendErrorMessage(ZERO);
+            return 0;
         }
+        if (factor < 0) {
+            factor = -1 / factor;
+        }
+        for (Entity entity : entities) {
+            ShinsuStats.get(entity).multiplyBaseTension(factor);
+            source.sendFeedback(new TranslationTextComponent(TENSION, entity.getDisplayName(), factor), true);
+        }
+        return entities.size();
+    }
+
+    private static int changeTechniqueLevel(CommandSource source, Collection<? extends Entity> entities, ShinsuTechnique technique) {
+        return changeTechniqueLevel(source, entities, technique, 1);
+    }
+
+    private static int changeTechniqueLevel(CommandSource source, Collection<? extends Entity> entities, ShinsuTechnique technique, int change) {
+        for (Entity entity : entities) {
+            ShinsuStats stats = ShinsuStats.get(entity);
+            stats.addKnownTechnique(technique, change);
+            if (entity instanceof ServerPlayerEntity) {
+                Map<ShinsuTechnique, Integer> known = new EnumMap<>(ShinsuTechnique.class);
+                for (ShinsuTechnique t : ShinsuTechnique.values()) {
+                    known.put(t, stats.getTechniqueLevel(t));
+                }
+                TowerOfGod.CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity), new UpdateClientKnownPacket(known));
+            }
+            source.sendFeedback(new TranslationTextComponent(TECHNIQUE, entity.getDisplayName(), technique.getText(), change), true);
+        }
+        return entities.size();
     }
 }
