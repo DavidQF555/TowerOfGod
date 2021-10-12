2261145438271c9ed1e856f91c157a2d1127d22c
DavidQF555 <davidfeng2003@gmail.com>
2021-10-12 16:18:32 -0700

    Moved Item Color Classes

--- src/main/java/io/github/davidqf555/minecraft/towerofgod/common/items/DeviceItem.java
+++ src/main/java/io/github/davidqf555/minecraft/towerofgod/common/items/DeviceItem.java
@@ -1,5 +1,6 @@
 package io.github.davidqf555.minecraft.towerofgod.common.items;
 
+import io.github.davidqf555.minecraft.towerofgod.client.render.DeviceItemColor;
 import io.github.davidqf555.minecraft.towerofgod.common.TowerOfGod;
 import io.github.davidqf555.minecraft.towerofgod.common.entities.devices.FlyingDevice;
 import net.minecraft.advancements.CriteriaTriggers;
