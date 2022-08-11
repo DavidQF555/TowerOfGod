3d8ea9e1ff51a8e192b71ed8e58f7e7e1f1e4199
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2022-08-11 09:21:47 -0700

    Improved Spear Entity Mechanics

--- src/main/java/io/github/davidqf555/minecraft/towerofgod/common/entities/SpearEntity.java
+++ src/main/java/io/github/davidqf555/minecraft/towerofgod/common/entities/SpearEntity.java
@@ -1,7 +1,10 @@
 package io.github.davidqf555.minecraft.towerofgod.common.entities;
 
 import io.github.davidqf555.minecraft.towerofgod.common.RegistryHandler;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.CompoundNBT;
@@ -9,6 +12,10 @@ import net.minecraft.network.IPacket;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.network.NetworkHooks;
@@ -62,6 +69,33 @@ public class SpearEntity extends AbstractArrowEntity {
     public void writeAdditional(CompoundNBT compound) {
         super.writeAdditional(compound);
         compound.put("Stack", getArrowStack().write(new CompoundNBT()));
+    }
+
+    @Override
+    protected void onEntityHit(EntityRayTraceResult result) {
+        Entity target = result.getEntity();
+        ItemStack stack = getArrowStack();
+        Vector3d motion = getMotion();
+        double damage = (((TieredItem) stack.getItem()).getTier().getAttackDamage() * 2 + 2) * motion.length();
+        if (target instanceof LivingEntity) {
+            damage += EnchantmentHelper.getModifierForCreature(stack, ((LivingEntity) target).getCreatureAttribute());
+        }
+        Entity shooter = getShooter();
+        DamageSource source = DamageSource.causeTridentDamage(this, shooter == null ? this : shooter);
+        if (target.attackEntityFrom(source, (float) damage)) {
+            if (target.getType() == EntityType.ENDERMAN) {
+                return;
+            }
+            if (target instanceof LivingEntity) {
+                if (shooter instanceof LivingEntity) {
+                    EnchantmentHelper.applyThornEnchantments((LivingEntity) target, shooter);
+                    EnchantmentHelper.applyArthropodEnchantments((LivingEntity) shooter, target);
+                }
+                arrowHit((LivingEntity) target);
+            }
+        }
+        setMotion(motion.mul(-0.01, -0.1, -0.01));
+        playSound(SoundEvents.ITEM_TRIDENT_HIT, 1, 1);
     }
 
     @Override
