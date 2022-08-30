6350374b7bd90df3a9d1a5e7abe5b9a93e30fa0a
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2022-08-30 08:43:17 -0700

    Improved Suspendium Ore Placement

--- src/main/java/io/github/davidqf555/minecraft/towerofgod/registration/WorldGenRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/towerofgod/registration/WorldGenRegistry.java
@@ -7,9 +7,14 @@ import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.heightproviders.UniformHeight;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -39,7 +44,7 @@ public final class WorldGenRegistry {
         public static void onFMLCommonSetup(FMLCommonSetupEvent event) {
             event.enqueueWork(() -> {
                 Holder<ConfiguredFeature<OreConfiguration, ?>> configured = FeatureUtils.register(new ResourceLocation(TowerOfGod.MOD_ID, "suspendium_ore").toString(), Feature.ORE, new OreConfiguration(OreFeatures.NATURAL_STONE, BlockRegistry.SUSPENDIUM_ORE.get().defaultBlockState(), 8));
-                SUSPENDIUM_ORE = PlacementUtils.register(new ResourceLocation(TowerOfGod.MOD_ID, "suspendium_ore").toString(), configured);
+                SUSPENDIUM_ORE = PlacementUtils.register(new ResourceLocation(TowerOfGod.MOD_ID, "suspendium_ore").toString(), configured, HeightRangePlacement.of(UniformHeight.of(VerticalAnchor.absolute(17), VerticalAnchor.absolute(117))), InSquarePlacement.spread(), CountPlacement.of(3));
             });
         }
     }
