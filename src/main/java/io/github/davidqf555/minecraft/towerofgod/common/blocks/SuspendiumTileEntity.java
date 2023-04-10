9224a4ee48523f3965a3838d7073dd08ef4145eb
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2023-04-09 22:27:20 -0500

    Improved Suspendium Block Targeting

--- src/main/java/io/github/davidqf555/minecraft/towerofgod/common/blocks/SuspendiumTileEntity.java
+++ src/main/java/io/github/davidqf555/minecraft/towerofgod/common/blocks/SuspendiumTileEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EntityPredicates;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -26,7 +27,7 @@ public class SuspendiumTileEntity extends TileEntity implements ITickableTileEnt
         if (world != null && world.getGameTime() % PERIOD == 0) {
             BlockPos pos = getBlockPos();
             AxisAlignedBB bounds = AxisAlignedBB.ofSize(RADIUS * 2, RADIUS * 2, RADIUS * 2).move(pos);
-            for (LivingEntity entity : world.getEntitiesOfClass(LivingEntity.class, bounds, entity -> entity.distanceToSqr(pos.getX(), pos.getY(), pos.getZ()) <= RADIUS * RADIUS)) {
+            for (LivingEntity entity : world.getEntitiesOfClass(LivingEntity.class, bounds, entity -> EntityPredicates.NO_CREATIVE_OR_SPECTATOR.test(entity) && entity.distanceToSqr(pos.getX(), pos.getY(), pos.getZ()) <= RADIUS * RADIUS)) {
                 entity.addEffect(new EffectInstance(Effects.LEVITATION, PERIOD, LEVEL - 1));
             }
         }
